daee6fda86d33fc7317489c4e09e1e2f0c4806b6
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-07-10 21:35:20 +0530

    chore: blahblah

--- content-script.js
+++ content-script.js
@@ -1,41 +1,39 @@
 //
 // https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
 //
+// Function to inject the script
 function injectScript() {
   const script = document.createElement("script");
   script.src = chrome.runtime.getURL("comentario.js");
   script.defer = true;
-  document.documentElement.appendChild(script); // <--- no err now
-  // chrome.scripting
-  //   .executeScript({
-  //     target: { tabId: getTabId(), allFrames: true },
-  //     files: ["comentario.js"],
-  //   })
-  //   .then(() => console.log("script injected in all frames"));
 
-  // (async () => {
-  //   // see the note below on how to choose currentWindow or lastFocusedWindow
-  //   const [tab] = await chrome.tabs.query({
-  //     active: true,
-  //     lastFocusedWindow: true,
-  //   });
-  //   console.log(tab.url);
-  //   // ..........
-  // })();
-  //
-  // <script defer src="http://13.201.77.10:8080/comentario.js"></script> thanks
-  // <comentario-comments></comentario-comments>
-  //target element not found a rha
+  const appendScript = () => {
+    if (document.head) {
+      document.head.appendChild(script);
+    } else {
+      requestAnimationFrame(appendScript);
+    }
+  };
 
-  console.log(window.location.href);
-  const comentarioElement = document.createElement("comentario-comments");
-  const targetElement = document.querySelector(".overflow-x-clip");
-  if (targetElement) {
-    targetElement.appendChild(comentarioElement);
-  } else {
-    console.warn("Target element not found");
-  }
+  appendScript();
+
+  // Create the custom element after the script is loaded
+  script.onload = () => {
+    const comentarioElement = document.createElement("comentario-comments");
+
+    const checkAndInject = () => {
+      const targetElement = document.querySelector(
+        "#content > div > div > div.app-body-wrapper > div > div > div.content-wrapper--MF5LS > div",
+      ); // Adjust the selector as needed
+      if (targetElement) {
+        targetElement.appendChild(comentarioElement);
+      } else {
+        requestAnimationFrame(checkAndInject);
+      }
+    };
+
+    checkAndInject();
+  };
 }
-
 // Inject the script
 injectScript();
