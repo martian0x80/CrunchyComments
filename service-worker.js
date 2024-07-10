aa491d2d69c0925953d1dd92dc31b4bfbecd39e0
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-07-10 20:56:31 +0530

    my world

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("onInstalled...");
   // chrome.storage.sync.set({ color: "#3aa757" }, () => {
-  //  console.log("The color is green.");
+  console.log("The color is green.");
 });
 
 // document.querySelector("#content > div > div > div.app-body-wrapper > div > div > div.content-wrapper--MF5LS > div")
