7ed3d2951e0977d6fd9b5590f6de4d30bb688f96
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-07-16 16:38:50 +0530

    chore: test code

--- content-script.js
+++ content-script.js
@@ -19,6 +19,7 @@ function injectScript() {
   document.addEventListener("readystatechange", (event) => {
     if (document.readyState === "complete") {
       checkAndInject();
+      // setupMutationObserver();
       checkAndUpdate();
     }
   });
@@ -26,6 +27,10 @@ function injectScript() {
   const checkAndInject = () => {
     const targetElement = document.querySelector(".app-body-wrapper");
     if (targetElement) {
+      let oldElement = document.querySelector("comentario-comments");
+      if (oldElement) {
+        oldElement.remove();
+      }
       comentarioElement = document.createElement("comentario-comments");
       comentarioElement.setAttribute("max-level", "3");
       targetElement.insertAdjacentElement("afterend", comentarioElement);
@@ -34,14 +39,54 @@ function injectScript() {
     }
   };
 
+  (() => {
+    let oldPushState = history.pushState;
+    history.pushState = function pushState() {
+      let ret = oldPushState.apply(this, arguments);
+      window.dispatchEvent(new Event("pushstate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+
+    let oldReplaceState = history.replaceState;
+    history.replaceState = function replaceState() {
+      let ret = oldReplaceState.apply(this, arguments);
+      window.dispatchEvent(new Event("replacestate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+
+    window.addEventListener("popstate", () => {
+      window.dispatchEvent(new Event("locationchange"));
+    });
+  })();
+
   const checkAndUpdate = () => {
+    // Only supports Chrome
     window.navigation.addEventListener("navigate", (event) => {
       document.getElementsByTagName("comentario-comments")[0].remove();
       setTimeout(() => {
         checkAndInject();
       }, 500);
     });
+
+    window.addEventListener("locationchange", (event) => {
+      document.getElementsByTagName("comentario-comments")[0].remove();
+      setTimeout(() => {
+        checkAndInject();
+      }, 500);
+    });
   };
+
+  // const setupMutationObserver = () => {
+  //   var previousUrl = "";
+  //   var observer = new MutationObserver(function (mutations) {
+  //     if (location.href !== previousUrl) {
+  //       previousUrl = location.href;
+  //       console.log(`URL changed to ${location.href}`);
+  //     }
+  //   });
+  // };
 }
 // Inject the script
 injectScript();
