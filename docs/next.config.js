e2b0b73fcfbb61b15f9840369103ac2dc8e53e36
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-11-18 14:11:08 +0530

    chore: webpack config messing with me

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config, { dev }) => {
-		// Disable cache in development
-		if (dev) {
-			config.cache = false
-		}
-		return config
-	},
+	output: 'export',
+  distDir: './out',
+  images: {
+    unoptimized: true,
+},
 }
 
 export default nextConfig
